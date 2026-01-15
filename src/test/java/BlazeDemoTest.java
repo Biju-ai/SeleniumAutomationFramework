9597fa3dec67c8b29cc3bbfeaefcb9dae061d404
Biju-ai <bijushrestha768@gmail.com>
2026-01-15 14:22:54 +0545

    BlazeTest

---
+++ src/test/java/BlazeDemoTest.java
@@ -0,0 +1,21 @@
+import org.example.base.BaseTest;
+import org.example.base.BlazeDemo;
+import org.example.pages.BlazeDemoClass;
+import org.testng.annotations.Test;
+
+public class BlazeDemoTest extends BlazeDemo {
+    @Test
+    public void BlazeDemoTest() throws InterruptedException {
+        BlazeDemoClass blazeDemoClass = new BlazeDemoClass(driver);
+        blazeDemoClass.selectData1();
+        blazeDemoClass.getOption1();
+        blazeDemoClass.selectData2();
+        blazeDemoClass.getOption2();
+        blazeDemoClass.selectButton();
+        blazeDemoClass.switchingToNextWindow();
+        blazeDemoClass.firstName("biju bro");
+        blazeDemoClass.setOnCard("122343");
+
+
+    }
+}
